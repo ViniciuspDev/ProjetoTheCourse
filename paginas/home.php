4668bfa8d9cd3160615ea2a854dc41a130098d7b
Vinicius <viniciuspinheirotecnologia@gmail.com>
2026-04-15 23:01:42 -0300

    Adicionando CTA pra página home

--- paginas/home.php
+++ paginas/home.php
@@ -2,6 +2,16 @@
     <h1>Comece Agora!</h1>
     <p>Venha aprender inglês de forma prática e eficiente, com métodos inovadores e professores qualificados.</p>
 
+    <div style="text-align: center; margin-top: 40px;">
+
+
+    
+        <a href="index.php?p=contato" class="btn-cta">Quero Saber Mais</a><br>
+        <br>
+        <p style="margin-top: 20px; color: white;">Tem interesse em aprender inglês com professores capacitados e metodologia eficaz?</p>
+        <br>
+    </div>
+    
 
     
 </section>
